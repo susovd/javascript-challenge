37482f425ff7409ffd2a3e7fa38164262bef7e11
susovd <dhakalsusov@gmail.com>
2020-09-29 12:47:47 +1000

    part 1 assignment

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,50 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// get a reference to the table body
+
+var tableBody = d3.select("tbody");
+
+//create a function that refreshes the table and deletes all existing data and create a new table with a new data
+
+
+function refreshTable(Data) {
+  tableBody.html("");
+  
+  Data.forEach(function(Row){
+
+    //create "tr" tag for every row 
+    var row = tableBody.append("tr");
+
+    //to separate the key and value
+    Object.entries(Row).forEach(function([key, value]) {
+
+      //create td tag for every cell
+      var cell = row.append("td");
+      cell.text(value);
+
+    });
+
+  });
+
+};
+
+function chosenDate() {
+  var inputDate = d3.select("#datetime").property("value");
+  console.log(inputDate)
+
+  var filteredData = tableData.filter(row => row.datetime ===inputDate);
+  refreshTable(filteredData);
+  
+};
+
+//Select the button
+
+var button = d3.select("#filter-btn");
+
+//create event handlers
+
+button.on("click", chosenDate);
+
+//call the table refresher function
+refreshTable(tableData);
